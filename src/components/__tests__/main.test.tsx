8e771223060dc3dbc84108416cd276c1e8c17928
J Kleuters <jkleuters@broadsight.nl>
2025-08-31 21:45:44 +0200

    main test toegevoegd

---
+++ src/components/__tests__/main.test.tsx
@@ -0,0 +1,18 @@
+// __tests__/App.test.tsx
+
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect} from 'vitest'
+import  App from '../../App'
+
+describe('App', () => {git 
+  it('toont de hoofdtitel', async () => {
+    render(<App />)
+
+    const heading = await screen.findByRole('heading', {
+        level: 1,
+        name: (content) => content.replace(/\s+/g, ' ').includes('Mijn oma zei vroeger altijd:')
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+})
